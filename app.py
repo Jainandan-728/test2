31a28d281ac268d28483d48d4b090df405cc5b9b
jainandansl <nandan56562@gmail.com>
2026-05-13 10:33:45 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! Docker is running is this python app successfully"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
